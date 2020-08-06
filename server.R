b5402d22ab5041b9c5daca639aacc0c484954741
Carlo <carlo.demusis@gmail.com>
2020-08-06 19:23:29 -0400

    Add files via upload

--- server.R
+++ server.R
@@ -1,7 +1,33 @@
-# library(shinydashboard)
-library(FuzzyR)
 
-# setwd("D:/OneDrive/arincendio/fuzzyIncedio")
+# library(shinydashboard)
+# install.packages('qlcMatrix')
+
+library(FuzzyR)
+library(mc2d)
+library(propagate)
+library(qlcMatrix)
+library(utils)
+
+
+# setwd("D:/OneDrive/Fuzzy/fuzzyIncendio")
+
+efeitoEvento <- function(valores, p, correlacoes, medias_efeitos, variancias_efeitos) {
+    nrep <- 1
+    aux_ber <- rbern(n=nrep, prob=p)
+    
+    covariancias <- cor2cov(correlacoes, variancias_efeitos)
+    covariancias <- as.vector(covariancias)
+    
+    aux_efeitos <- aux_ber*rmultinormal(nrep, medias_efeitos, covariancias)
+    aux_efeitos[aux_efeitos==0] <- 1
+    var_cons_efeito <- sweep(aux_efeitos, MARGIN = 2, valores, '*')
+    
+    return(var_cons_efeito)
+}
+
+# 
+# Servidor.
+#
 
 server <- function(input, output) {
     criaFuzzy <- reactive({
@@ -10,18 +36,19 @@ server <- function(input, output) {
         m1_fis <<- newfis('C')
         
         # Adiciona variáveis.
-        m1_fis <<- addvar(m1_fis, 'input', 'A', c(0, 100))
-        m1_fis <<- addvar(m1_fis, 'input', 'B', c(0, 100))
-        m1_fis <<- addvar(m1_fis, 'output', 'C', c(0, 100))
+        m1_fis <<- FuzzyR::addvar(m1_fis, 'input', 'Cobertura vegetal', c(0, 100))
+        m1_fis <<- FuzzyR::addvar(m1_fis, 'input', 'Relevo', c(0, 100))
+        m1_fis <<- FuzzyR::addvar(m1_fis, 'input', 'Proximidade da população', c(0, 1000))
+        m1_fis <<- FuzzyR::addvar(m1_fis, 'output', 'Risco de incêndio', c(0, 100))
         
         # Adiciona funções de pertença.
-        # A.
+        # Cobertura vegetal.
         m1_fis <<-
             addmf(
                 m1_fis,
                 'input',
                 1,
-                'baixo',
+                'baixa',
                 'trapmf',
                 c(input$b_1_1, input$b_1_2, input$b_1_3, input$b_1_4)
             )
@@ -30,7 +57,7 @@ server <- function(input, output) {
                 m1_fis,
                 'input',
                 1,
-                'médio',
+                'média',
                 'trapmf',
                 c(input$m_1_1, input$m_1_2, input$m_1_3, input$m_1_4)
             )
@@ -39,17 +66,18 @@ server <- function(input, output) {
                 m1_fis,
                 'input',
                 1,
-                'alto',
+                'alta',
                 'trapmf',
                 c(input$a_1_1, input$a_1_2, input$a_1_3, input$a_1_4)
             )
-        # B.
+        
+        # Relevo.
         m1_fis <<-
             addmf(
                 m1_fis,
                 'input',
                 2,
-                'baixo',
+                'Plano',
                 'trapmf',
                 c(input$b_2_1, input$b_2_2, input$b_2_3, input$b_2_4)
             )
@@ -58,26 +86,93 @@ server <- function(input, output) {
                 m1_fis,
                 'input',
                 2,
-                'médio',
+                'Rampa ou colina',
                 'trapmf',
-                c(input$m_2_1, input$m_2_2, input$m_2_3, input$m_2_4)
+                c(input$mb_2_1, input$mb_2_2, input$mb_2_3, input$mb_2_4)
             )
         m1_fis <<-
             addmf(
                 m1_fis,
                 'input',
                 2,
-                'alto',
+                'Morrote',
+                'trapmf',
+                c(input$ma_2_1, input$ma_2_2, input$ma_2_3, input$ma_2_4)
+            )
+        m1_fis <<-
+            addmf(
+                m1_fis,
+                'input',
+                2,
+                'Morro com encosta suave',
                 'trapmf',
                 c(input$a_2_1, input$a_2_2, input$a_2_3, input$a_2_4)
             )
-        # C.
+        m1_fis <<-
+            addmf(
+                m1_fis,
+                'input',
+                2,
+                'Morro com encosta forte',
+                'trapmf',
+                c(input$ma_2_1, input$ma_2_2, input$ma_2_3, input$ma_2_4)
+            )
+        
+        # Proximidade.
+        m1_fis <<-
+            addmf(
+                m1_fis,
+                'input',
+                3,
+                'Na área',
+                'trapmf',
+                c(input$b_2_1, input$b_2_2, input$b_2_3, input$b_2_4)
+            )
+        m1_fis <<-
+            addmf(
+                m1_fis,
+                'input',
+                3,
+                'Próxima',
+                'trapmf',
+                c(input$mb_2_1, input$mb_2_2, input$mb_2_3, input$mb_2_4)
+            )
+        m1_fis <<-
+            addmf(
+                m1_fis,
+                'input',
+                3,
+                'Média',
+                'trapmf',
+                c(input$ma_2_1, input$ma_2_2, input$ma_2_3, input$ma_2_4)
+            )
+        m1_fis <<-
+            addmf(
+                m1_fis,
+                'input',
+                3,
+                'Distante',
+                'trapmf',
+                c(input$a_2_1, input$a_2_2, input$a_2_3, input$a_2_4)
+            )
+        
+        
+        # Risco de incêndio.
         m1_fis <<-
             addmf(
                 m1_fis,
                 'output',
                 1,
-                'baixo',
+                'Muito baixo',
+                'trapmf',
+                c(input$mb_3_1, input$mb_3_2, input$mb_3_3, input$mb_3_4)
+            )
+        m1_fis <<-
+            addmf(
+                m1_fis,
+                'output',
+                1,
+                'Baixo',
                 'trapmf',
                 c(input$b_3_1, input$b_3_2, input$b_3_3, input$b_3_4)
             )
@@ -86,7 +181,7 @@ server <- function(input, output) {
                 m1_fis,
                 'output',
                 1,
-                'médio',
+                'Médio',
                 'trapmf',
                 c(input$m_3_1, input$m_3_2, input$m_3_3, input$m_3_4)
             )
@@ -95,9 +190,18 @@ server <- function(input, output) {
                 m1_fis,
                 'output',
                 1,
-                'alto',
+                'Alto',
                 'trapmf',
                 c(input$a_3_1, input$a_3_2, input$a_3_3, input$a_3_4)
+            )
+        m1_fis <<-
+            addmf(
+                m1_fis,
+                'output',
+                1,
+                'Muito alto',
+                'trapmf',
+                c(input$ma_3_1, input$ma_3_2, input$ma_3_3, input$ma_3_4)
             )
         
         # Regras.
@@ -110,41 +214,52 @@ server <- function(input, output) {
             m1_fis <<- addrule (m1_fis, as.matrix(lista_regras[,2:6]))
         } else {
             lista_regras <- rbind(
-                # A  B  C   peso operador (1 - AND, 2 OR)
-                c(1, 1, 1,  1,   1),
-                c(1, 2, 2,  1,   1),
-                c(1, 3, 3,  1,   1),
+                # A  B  C  D peso operador (1 - AND, 2 OR)
+                c(1, 1, 1, 1,   1,       1),
+                c(1, 2, 2, 1,   1,       1),
+                c(1, 3, 3, 1,   1,       1),
                 
-                c(2, 1, 2,  1,   1),
-                c(2, 2, 2,  1,   1),
-                c(2, 3, 3,  1,   1),
+                c(2, 1, 2, 1,   1,       1),
+                c(2, 2, 2, 1,   1,       1),
+                c(2, 3, 3, 1,   1,       1),
                 
-                c(3, 1, 3,  1,   1),
-                c(3, 2, 3,  1,   1),
-                c(3, 3, 3,  1,   1)
+                c(3, 1, 3, 1,   1,       1),
+                c(3, 2, 3, 1,   1,       1),
+                c(3, 3, 3, 1,   1,       1)
             )
-            m1_fis <<- addrule (m1_fis, lista_regras)
+            
+            lista_regras <- expand.grid(1:3, 1:5, 1:4)
+            
+            lista_regras$c <- rowMax(as.matrix(lista_regras))@x
+            lista_regras$peso <- 1
+            lista_regras$operador <- 1
+            m1_fis <<- addrule(m1_fis, as.matrix(lista_regras))
         }
     })
     
-    output$fuzzy_A <- renderPlot({
+    output$cobertura_vegetal_fuzzy <- renderPlot({
         criaFuzzy()
         plotmf(m1_fis, "input" , 1)
     })
     
-    output$fuzzy_B <- renderPlot({
+    output$relevo_fuzzy <- renderPlot({
         criaFuzzy()
         plotmf(m1_fis, "input" , 2)
     })
     
-    output$fuzzy_C <- renderPlot({
+    output$proximidade_fuzzy <- renderPlot({
+        criaFuzzy()
+        plotmf(m1_fis, "input" , 3)
+    })
+    
+    output$risco_incendio_fuzzy <- renderPlot({
         criaFuzzy()
         plotmf(m1_fis, "output", 1)
     })
     
-    output$fuzificacao_C <- renderPlot({
+    output$risco_incendio_fuzificacao <- renderPlot({
         criaFuzzy()
-        res <- evalfis(c(input$v_1, input$v_2), m1_fis, draw = FALSE)
+        res <- evalfis(c(input$v_1, input$v_2, input$v_3), m1_fis, draw = FALSE)
         nome_var = m1_fis$output[[1]]$name
         plot(D_x, D_y, type = "l", col = "blue", lwd = 3,
              xlab=nome_var,
@@ -163,13 +278,36 @@ server <- function(input, output) {
         texto <- gsub(" and ", " e ", texto)
         texto <- gsub(" or ", " ou ", texto)
         texto <- gsub(" then ", " então ", texto)
-        texto
+        HTML(texto)
+    })
+    
+    output$histograma_mmc <- renderPlot({
+        res_mmc <- data.frame(va=double(),
+                              vb=double(),
+                              stringsAsFactors=FALSE)
+        for (n in 1:input$n_mmc) {
+            # Avalia o efeito do evento 1 (por hora).
+            aux_res <- efeitoEvento(
+                valores = c(input$v_1, input$v_2, input$v_3),
+                p = input$p_1,
+                correlacoes = matrix(c(             1, input$corr_1_2, input$corr_1_3,
+                                       input$corr_1_2,              1, input$corr_2_3,
+                                       input$corr_1_3, input$corr_2_3,              1), ncol = 3),
+                medias_efeitos = c(input$e_1_1, input$e_1_2, input$e_1_3),
+                variancias_efeitos = c(input$v_1_1, input$v_1_2, input$v_1_3)
+            )
+            
+            res_mmc <- rbind(res_mmc, aux_res)
+        }
+        res <- evalfis(as.matrix(res_mmc), m1_fis, draw = FALSE)
+        hist(res)
     })
     
     output$fisArquivo <- downloadHandler(filename = 'fuzzy.fis', 
                                          content = function(arqu){
                                          writefis(m1_fis, arqu)
                                          })
+    
 }
 
 # shinyApp(ui, server)
--- ui.R
+++ ui.R
@@ -10,9 +10,14 @@ shinyUI(dashboardPage(
     dashboardSidebar(
         sidebarMenu(
             menuItem(
-                "Fuzificação",
-                tabName = "fuzificacao",
+                "Lógica fuzzy",
+                tabName = "fuzzy",
                 icon = icon("dashboard")
+            ),
+            menuItem(
+                "Método de Monte Carlo",
+                tabName = "mmc",
+                icon = icon("spinner")
             ),
             menuItem(
                 "Instruções",
@@ -33,113 +38,176 @@ shinyUI(dashboardPage(
     #
     dashboardBody(tabItems(
         # Fuzificação
-        tabItem(tabName = "fuzificacao",
+        tabItem(tabName = "fuzzy",
                 fluidRow(
                     tabBox(
-                        width = 4,
-                        title = "A (antecedente)",
+                        width = 3,
+                        title = "Cobertura vegetal",
                         tabPanel(
-                            title = "Baixo",
-                            sliderInput("b_1_1", "Mínimo (a):", 0, 100, 0),
+                            title = "Baixa",
+                            sliderInput("b_1_1", "a:", 0, 100, 0),
                             sliderInput("b_1_2", "b:", 0, 100, 0),
                             sliderInput("b_1_3", "c:", 0, 100, 25),
-                            sliderInput("b_1_4", "Máximo (d):", 0, 100, 50)
+                            sliderInput("b_1_4", "d:", 0, 100, 50)
                         ),
                         tabPanel(
-                            title = "Médio",
-                            sliderInput("m_1_1", "Mínimo (a):", 0, 100, 0),
+                            title = "Média",
+                            sliderInput("m_1_1", "a:", 0, 100, 0),
                             sliderInput("m_1_2", "b:", 0, 100, 25),
                             sliderInput("m_1_3", "c:", 0, 100, 75),
-                            sliderInput("m_1_4", "Máximo (d):", 0, 100, 100)
+                            sliderInput("m_1_4", "d:", 0, 100, 100)
                         ),
                         tabPanel(
-                            title = "Alto",
-                            sliderInput("a_1_1", "Mínimo (a):", 0, 100, 50),
+                            title = "Alta",
+                            sliderInput("a_1_1", "a:", 0, 100, 50),
                             sliderInput("a_1_2", "b:", 0, 100, 75),
                             sliderInput("a_1_3", "c:", 0, 100, 100),
-                            sliderInput("a_1_4", "Máximo (d):", 0, 100, 100)
+                            sliderInput("a_1_4", "d:", 0, 100, 100)
                         ),
                         tabPanel("Gráfico",
-                                 plotOutput("fuzzy_A", height = 500))
+                                 plotOutput("cobertura_vegetal_fuzzy", height = 500))
                     ),
                     
                     tabBox(
-                        width = 4,
-                        title = "B (antecedente)",
+                        width = 3,
+                        title = "Relevo (%)",
                         tabPanel(
-                            title = "Baixo",
-                            sliderInput("b_2_1", "Mínimo (a):", 0, 100, 0),
-                            sliderInput("b_2_2", "b:", 0, 100, 0),
-                            sliderInput("b_2_3", "c:", 0, 100, 25),
-                            sliderInput("b_2_4", "Máximo (d):", 0, 100, 50)
+                            title = "Plano",
+                            sliderInput("b_2_1", "a:", 0, 10, 0),
+                            sliderInput("b_2_2", "b:", 0, 10, 0),
+                            sliderInput("b_2_3", "c:", 0, 10, 5),
+                            sliderInput("b_2_4", "d", 0, 10, 5)
                         ),
                         tabPanel(
-                            title = "Médio",
-                            sliderInput("m_2_1", "Mínimo (a):", 0, 100, 0),
-                            sliderInput("m_2_2", "b:", 0, 100, 25),
-                            sliderInput("m_2_3", "c:", 0, 100, 75),
-                            sliderInput("m_2_4", "Máximo (d):", 0, 100, 100)
+                            title = "Rampa ou colina",
+                            sliderInput("mb_2_1", "a:", 0, 100, 0),
+                            sliderInput("mb_2_2", "b:", 0, 100, 15),
+                            sliderInput("mb_2_3", "c:", 0, 100, 25),
+                            sliderInput("mb_2_4", "d", 0, 100, 50)
                         ),
                         tabPanel(
-                            title = "Alto",
-                            sliderInput("a_2_1", "Mínimo (a):", 0, 100, 50),
+                            title = "Morrote",
+                            sliderInput("ma_2_1", "a:", 0, 100, 15),
+                            sliderInput("ma_2_2", "b:", 0, 100, 25),
+                            sliderInput("ma_2_3", "c:", 0, 100, 50),
+                            sliderInput("ma_2_4", "d", 0, 100, 750)
+                        ),
+                        tabPanel(
+                            title = "Morro com encosta suave",
+                            sliderInput("a_2_1", "a", 0, 100, 50),
                             sliderInput("a_2_2", "b:", 0, 100, 75),
                             sliderInput("a_2_3", "c:", 0, 100, 100),
-                            sliderInput("a_2_4", "Máximo (d):", 0, 100, 100)
+                            sliderInput("a_2_4", "d", 0, 100, 100)
+                        ),
+                        tabPanel(
+                            title = "Morro com encosta forte",
+                            sliderInput("ma_2_1", "a", 0, 100, 50),
+                            sliderInput("ma_2_2", "b:", 0, 100, 75),
+                            sliderInput("ma_2_3", "c:", 0, 100, 100),
+                            sliderInput("ma_2_4", "d", 0, 100, 100)
                         ),
                         tabPanel("Gráfico",
-                                 plotOutput("fuzzy_B", height = 500))
+                                 plotOutput("relevo_fuzzy", height = 500))
                     ),
                     
                     tabBox(
-                        width = 4,
-                        title = "C (consequente)",
+                        width = 3,
+                        title = "Proximidade de populacação (m)",
+                        tabPanel(
+                            title = "Na área",
+                            sliderInput("b_2_1", "a:", 0, 50, 0),
+                            sliderInput("b_2_2", "b:", 0, 50, 0),
+                            sliderInput("b_2_3", "c:", 0, 50, 25),
+                            sliderInput("b_2_4", "d", 0, 50, 25)
+                        ),
+                        tabPanel(
+                            title = "Próxima",
+                            sliderInput("mb_2_1", "a:", 0, 100, 0),
+                            sliderInput("mb_2_2", "b:", 0, 100, 15),
+                            sliderInput("mb_2_3", "c:", 0, 100, 25),
+                            sliderInput("mb_2_4", "d", 0, 100, 100)
+                        ),
+                        tabPanel(
+                            title = "Média",
+                            sliderInput("ma_2_1", "a:", 0, 500, 50),
+                            sliderInput("ma_2_2", "b:", 0, 500, 125),
+                            sliderInput("ma_2_3", "c:", 0, 500, 250),
+                            sliderInput("ma_2_4", "d", 0, 500, 500)
+                        ),
+                        tabPanel(
+                            title = "Distante",
+                            sliderInput("ma_2_1", "a", 250, 1000, 50),
+                            sliderInput("ma_2_2", "b:", 250, 1000, 750),
+                            sliderInput("ma_2_3", "c:", 250, 1000, 1000),
+                            sliderInput("ma_2_4", "d", 250, 1000, 1000)
+                        ),
+                        tabPanel("Gráfico",
+                                 plotOutput("proximidade_fuzzy", height = 500))
+                    ),
+                    
+                    tabBox(
+                        width = 3,
+                        title = "Risco de incêndio",
+                        tabPanel(
+                            title = "Muito baixo",
+                            sliderInput("mb_3_1", "a:", 0, 100, 0),
+                            sliderInput("mb_3_2", "b:", 0, 100, 0),
+                            sliderInput("mb_3_3", "c:", 0, 100, 15),
+                            sliderInput("mb_3_4", "d:", 0, 100, 25)
+                        ),                        
                         tabPanel(
                             title = "Baixo",
-                            sliderInput("b_3_1", "Mínimo (a):", 0, 100, 0),
-                            sliderInput("b_3_2", "b:", 0, 100, 0),
+                            sliderInput("b_3_1", "a:", 0, 100, 0),
+                            sliderInput("b_3_2", "b:", 0, 100, 15),
                             sliderInput("b_3_3", "c:", 0, 100, 25),
-                            sliderInput("b_3_4", "Máximo (d):", 0, 100, 50)
+                            sliderInput("b_3_4", "d:", 0, 100, 50)
                         ),
                         tabPanel(
                             title = "Médio",
-                            sliderInput("m_3_1", "Mínimo (a):", 0, 100, 0),
+                            sliderInput("m_3_1", "a:", 0, 100, 0),
                             sliderInput("m_3_2", "b:", 0, 100, 25),
                             sliderInput("m_3_3", "c:", 0, 100, 75),
-                            sliderInput("m_3_4", "Máximo (d):", 0, 100, 100)
+                            sliderInput("m_3_4", "d:", 0, 100, 100)
                         ),
                         tabPanel(
                             title = "Alto",
-                            sliderInput("a_3_1", "Mínimo (a):", 0, 100, 50),
-                            sliderInput("a_3_2", "b:", 0, 100, 75),
-                            sliderInput("a_3_3", "c:", 0, 100, 100),
-                            sliderInput("a_3_4", "Máximo (d):", 0, 100, 100)
+                            sliderInput("a_3_1", "a:", 0, 100, 25),
+                            sliderInput("a_3_2", "b:", 0, 100, 50),
+                            sliderInput("a_3_3", "c:", 0, 100, 75),
+                            sliderInput("a_3_4", "d:", 0, 100, 100)
+                        ),
+                        tabPanel(
+                            title = "Muito alto",
+                            sliderInput("ma_3_1", "a:", 0, 100, 50),
+                            sliderInput("ma_3_2", "b:", 0, 100, 75),
+                            sliderInput("ma_3_3", "c:", 0, 100, 100),
+                            sliderInput("ma_3_4", "d:", 0, 100, 100)
                         ),
                         tabPanel("Gráfico",
-                                 plotOutput("fuzzy_C", height = 500))
+                                 plotOutput("risco_incendio_fuzzy", height = 500))
                     ),
                     
                     box(
-                        width = 4,
+                        width = 9,
                         status = "primary",
                         solidHeader = TRUE,
                         collapsible = TRUE,
-                        title = "Fuzificação - Consequente",
-                        plotOutput("fuzificacao_C", height = 500),
+                        title = "Fuzificação - Risco de incêndio",
+                        plotOutput("risco_incendio_fuzificacao", height = 500),
                         downloadButton('fisArquivo', label = "Baixar")
                     ),
                     box(
-                        width = 4,
+                        width = 3,
                         status = "primary",
                         solidHeader = TRUE,
                         collapsible = TRUE,
                         title = "Fuzificação - Antecedentes",
-                        sliderInput("v_1", "A:", 0, 100, 50),
-                        sliderInput("v_2", "B:", 0, 100, 50)
-                        
+                        sliderInput("v_1", "Cobertura vegetal:", 0, 100, 50),
+                        sliderInput("v_2", "Relevo:", 0, 100, 50),
+                        sliderInput("v_3", "Proximidade da população:", 0, 1000, 150)
                     ),
                     box(
-                        width = 4,
+                        width = 12,
                         status = "primary",
                         solidHeader = TRUE,
                         collapsible = TRUE,
@@ -157,14 +225,81 @@ shinyUI(dashboardPage(
                         textOutput("regrasTexto")
                     )
                 ),),
-        
+
+        # Método de Monte Carlo.
+        tabItem(
+            tabName = 'mmc',
+            box(
+                width = 4,
+                status = "primary",
+                solidHeader = TRUE,
+                collapsible = TRUE,
+                title = "Evento - Raio",
+                sliderInput("p_1", "Probabilidade:", 0, 1, 0.5, 0.1),
+                tags$hr(),
+                sliderInput("e_1_1", "Efeito esperado na Cobertura vegetal:", -1, 1, 0.3, 0.1),
+                sliderInput("v_1_1", "Variabilidade do efeito na Cobertura vegetal:", 0, 1, 0.2, 0.1),
+                tags$hr(),
+                sliderInput("e_1_2", "Efeito esperado no Relevo:", -1, 1, -0.1, 0.1),
+                sliderInput("v_1_2", "Variabilidade do efeito no Relevo:", 0, 1, 0.2, 0.1),
+                tags$hr(),
+                sliderInput("e_1_3", "Efeito esperado na Vizinhança:", -1, 1, 0.3, 0.1),
+                sliderInput("v_1_3", "Variabilidade do efeito na Vizinhança:", 0, 1, 0.4, 0.1)                
+            ),
+            box(
+                width = 4,
+                status = "primary",
+                solidHeader = TRUE,
+                collapsible = TRUE,
+                title = "Evento - Bituca de cigarro",
+                sliderInput("p_2", "Probabilidade:", 0, 1, 0.5, 0.1),
+                tags$hr(),
+                sliderInput("e_2_1", "Efeito esperado na Cobertura vegetal:", -1, 1, 0, 0.1),
+                sliderInput("v_2_1", "Variabilidade do efeito na Cobertura vegetal:", 0, 1, 0, 0.1),
+                tags$hr(),
+                sliderInput("e_2_2", "Efeito esperado no Relevo:", -1, 1, 0, 0.1),
+                sliderInput("e_2_2", "Variabilidade do efeito no Relevo:", 0, 1, 0, 0.1)
+            ),
+            box(
+                width = 4,
+                status = "primary",
+                solidHeader = TRUE,
+                collapsible = TRUE,
+                title = "Evento 3",
+                sliderInput("p_3", "Probabilidade:", 0, 1, 0.5, 0.1),
+                tags$hr(),
+                sliderInput("e_3_1", "Efeito esperado na Cobertura vegetal:", -1, 1, 0, 0.1),
+                sliderInput("v_3_1", "Variabilidade do efeito na Cobertura vegetal:", 0, 1, 0, 0.1),
+                tags$hr(),
+                sliderInput("e_3_2", "Efeito esperado no Relevo:", -1, 1, 0, 0.1),
+                sliderInput("e_3_2", "Variabilidade do efeito no Relevo:", 0, 1, 0, 0.1)
+            ),
+            box(
+                width = 4,
+                status = "primary",
+                solidHeader = TRUE,
+                collapsible = TRUE,
+                title = "Correlações",
+                sliderInput("corr_1_2", "Cobertura vegetal e Relevo:", -1, 1, -0.5, 0.1),
+                sliderInput("corr_1_3", "Cobertura vegetal e Proximidade:", -1, 1, 0.4, 0.1),
+                sliderInput("corr_2_3", "Proximidade e Relevo:", -1, 1, 0.3, 0.1)
+            ),
+            box(
+                width = 4,
+                status = "primary",
+                solidHeader = TRUE,
+                collapsible = TRUE,
+                title = "Simulações",
+                sliderInput("n_mmc", "N:", 10, 100, 15),
+                plotOutput("histograma_mmc", height = 500)
+                
+            ) 
+        ),
+                
         # Instruções.
         tabItem(
             tabName = "instrucoes",
-            h2("Fuzificação."),
-            h3(stri_rand_lipsum(1)),
-            h2("Defuzificação."),
-            h3(stri_rand_lipsum(1)),
+            includeMarkdown("ajuda.Rmd")
             
         )
     ))
